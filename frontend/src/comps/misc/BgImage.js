1ebef8d29288d935e2a886cc308af7f90cdf2e39
ritheesh-bs <reobs58@gmail.com>
2024-11-29 21:42:36 +0530

    Changed BG image for dark mode

--- frontend/src/comps/misc/BgImage.js
+++ frontend/src/comps/misc/BgImage.js
@@ -5,7 +5,7 @@ export default function BgImage() {
     return (
         <picture>
             <img 
-                src={LightBackgroundImage}
+                src={DarkBackgroundImage}
                 alt="Background image"
                 className="absolute top-0 left-0 h-dvh w-screen object-cover opacity-40 lg:opacity-10 pointer-events-none hidden dark:block"
                 aria-hidden="true"
