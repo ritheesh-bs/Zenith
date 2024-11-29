b3046ef435f57885f6566fed7fa934626801c8f4
ritheesh-bs <reobs58@gmail.com>
2024-11-29 21:49:18 +0530

    Made tiny UX improvements

--- frontend/src/comps/messages/MyMessage.js
+++ frontend/src/comps/messages/MyMessage.js
@@ -2,7 +2,7 @@ export default function MyMessage({message}){
 
 
     return (<div className="flex w-full justify-end">
-        <p className="flex bg-white-300 dark:bg-gray-400 border-[0.5px] border-slate-100 dark:border-gray-500 text-gray-700 dark:text-slate-300 min-w-[60px] max-w-[300px] lg:max-w-[500px] w-auto rounded-3xl rounded-br-none p-4">
+        <p className="flex bg-white-100 bg-opacity-20 dark:bg-gray-400 border-[0.5px] border-slate-100 dark:border-gray-500 text-gray-700 dark:text-slate-300 min-w-[60px] max-w-[300px] lg:max-w-[500px] w-auto rounded-3xl rounded-br-none p-4">
             { message }
         </p>
     </div>)
--- frontend/src/comps/misc/BgImage.js
+++ frontend/src/comps/misc/BgImage.js
@@ -10,6 +10,12 @@ export default function BgImage() {
                 className="absolute top-0 left-0 h-dvh w-screen object-cover opacity-40 lg:opacity-10 pointer-events-none hidden dark:block"
                 aria-hidden="true"
             />
+            <img 
+                src={LightBackgroundImage}
+                alt="Background image"
+                className="absolute top-0 rotate-180 left-0 h-dvh w-screen object-cover opacity-30 pointer-events-none dark:hidden"
+                aria-hidden="true"
+            />
         </picture>
     );
 }
